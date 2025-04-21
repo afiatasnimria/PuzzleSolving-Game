72e4e2ce7f404f49cfddcbeefaf3a77b1551f0b3
salahuddinselim <selimsalahuddin19@gmail.com>
2025-04-21 22:41:21 +0600

    fix
    fix the problem of scene change

--- demo1/src/main/java/com/example/demo1/HelloController.java
+++ demo1/src/main/java/com/example/demo1/HelloController.java
@@ -1,7 +1,14 @@
 package com.example.demo1;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class HelloController {
     @FXML
@@ -11,4 +18,14 @@ public class HelloController {
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
     }
+
+    public void LoginPage(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("loginPage.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setTitle("Login Page");
+        stage.setScene(scene);
+        stage.show();
+    }
+
 }
