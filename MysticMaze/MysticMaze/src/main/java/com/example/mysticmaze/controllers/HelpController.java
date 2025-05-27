62cfd58e8a9065ad35c8f6bafd9ce02822559921
Afia Tasnim Ria <afiatasnimria@gmail.com>
2025-05-27 11:42:21 +0600

    design update

--- MysticMaze/MysticMaze/src/main/java/com/example/mysticmaze/controllers/HelpController.java
+++ MysticMaze/MysticMaze/src/main/java/com/example/mysticmaze/controllers/HelpController.java
@@ -3,24 +3,22 @@ package com.example.mysticmaze.controllers;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import javafx.scene.Node;
 
 import java.io.IOException;
 
 public class HelpController {
 
     @FXML
-    private void handleBack(ActionEvent event) {
-        try {
-            Parent loginRoot = FXMLLoader.load(getClass().getResource("/com/example/mysticmaze/fxmls/HomePage.fxml"));
-            Scene loginScene = new Scene(loginRoot);
-            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-            stage.setScene(loginScene);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    public void handleBackButton(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/example/mysticmaze/fxmls/HelpPage.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setResizable(false);
+        stage.setTitle("Mystic Maze - Home");
+        stage.setScene(scene);
+        stage.show();
     }
 }
